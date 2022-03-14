df76b87ff6b47708fab023fe9b0413e6adfecb14
Matheus Moreira <matheusmoreira1324@hotmail.com>
2022-03-14 19:58:54 -0300

    sistema-condominio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import initScrollSuave from './modules/scrollSuave.js';
+import initAccordion from './modules/accordion.js';
+import initModal from './modules/modal.js'
+
+initScrollSuave();
+initAccordion();
+initModal();
